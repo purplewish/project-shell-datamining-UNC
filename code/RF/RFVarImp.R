9a55d53396810b223ab86cd209d3410d637e0d89
mqwu <send2mqwu@gmail.com>
2015-02-04 17:35:09 -0600

    Reformat

--- code/RF/RFVarImp.R
+++ code/RF/RFVarImp.R
@@ -25,16 +25,19 @@ plotRFVarImp <- function(rf.mod){
 		d$Predictor <- factor(as.character(d$Predictor),levels=rev(as.character(d$Predictor)))
 		rownames(d) <- NULL
   
-    	d[,2] <- d[,2]/abs(max(d[,2])) * 100  # normalize relative to the maximum
+    		d[,2] <- d[,2]/abs(max(d[,2])) * 100  # normalize relative to the maximum
 		abs.min <- abs(min(d[,2]))
 		
-    	g1 <- ggplot(data=d,aes_string(x="Predictor",y=ylb,group="Predictor")) + geom_bar(stat="identity", colour="#639f89", fill="#639f89") + theme_grey(base_size=fontsize)
-		if(ylb=="mda") g1 <- g1 + labs(y="Mean decrease in accuracy") else if(ylb=="mdg") g1 <- g1 + labs(y="Mean decrease in Gini")
+    		g1 <- ggplot(data=d,aes_string(x="Predictor",y=ylb,group="Predictor")) + geom_bar(stat="identity", colour="#639f89", fill="#639f89") + theme_grey(base_size=fontsize)
+
+		if(ylb=="mda") g1 <- g1 + labs(y="Mean decrease in accuracy") 
+		else if(ylb=="mdg") g1 <- g1 + labs(y="Mean decrease in Gini")
+
 		g1 <- g1 + theme(axis.title=element_text(size=25,face="bold"), 
                      axis.text.x=element_text(angle=0,hjust=1,vjust=0.4,colour='black'),
                      axis.text.y= element_text(colour='black', size=25)) + 
-          geom_hline(yintercept=abs.min,linetype="dashed",colour="black") + coord_flip()
-		print(g1)
+          	     geom_hline(yintercept=abs.min,linetype="dashed",colour="black") + coord_flip()
+	print(g1)
 	}
 
 	importancePlot(d=pred.acc, ylb="mda", 20)
